190d5b15e9a86dd1baeb2b4460ccfa7f7e0bac49
Anton Palermo <ANPBusinessmail@gmail.com>
2022-03-20 21:51:23 +0800

    added password regex

--- src/account/dto/create-user.dto.ts
+++ src/account/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator'
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength
+} from 'class-validator'
 import { IsTaken } from '../decorators/is-taken.decorator'
 
 export class CreateUserDto {
@@ -16,7 +22,18 @@ export class CreateUserDto {
   @IsTaken()
   email: string
 
+  /**
+   * Regex, in this case a password may contain:
+    - (?=.*?[A-Z]) : At least one upper case English letter
+    - (?=.*?[a-z]) : At least one lower case English letter
+    - (?=.*?[0-9]) : At least one digit
+    - (?=.*?[#?!@$ %^&*-]) : At least one special character or space
+    - .{8,} : Minimum eight in length
+   */
   @IsNotEmpty()
   @MinLength(8)
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, {
+    message: 'please provide a more complex password'
+  })
   password: string
 }
